1ddfa3c99838517b7f46dc7382d9fa775d90cf76
Elie Rakoto <deicide21@hotmail.fr>
2023-11-29 17:27:34 +0100

    done

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -127,6 +127,38 @@ const updateMovie = (req, res) => {
     });
 
 };
+const deleteMovie = (req, res) => {
+
+  const id = parseInt(req.params.id);
+
+
+  database
+
+    .query("delete from movies where id = ?", [id])
+
+    .then(([result]) => {
+
+      if (result.affectedRows === 0) {
+
+        res.sendStatus(404);
+
+      } else {
+
+        res.sendStatus(204);
+
+      }
+
+    })
+
+    .catch((err) => {
+
+      console.error(err);
+
+      res.sendStatus(500);
+
+    });
+
+};
 
 module.exports = {
 
@@ -136,6 +168,8 @@ module.exports = {
 
   postMovie,
 
-  updateMovie
+  updateMovie,
+   
+  deleteMovie
 
 };
